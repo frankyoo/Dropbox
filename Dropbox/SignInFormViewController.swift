b6f1088c7e769a138a3b9a5bafab58c63ff7ebfb
Frank <frank@lyft.com>
2016-02-07 03:26:05 -0800

    Added UITextFields and keyboard, also changed color of button text

--- Dropbox/SignInFormViewController.swift
+++ Dropbox/SignInFormViewController.swift
@@ -10,8 +10,25 @@ import UIKit
 
 class SignInFormViewController: UIViewController {
 
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
+ 
+    func keyboardWillShow(notification: NSNotification!) {
+        
+    }
+    
+    func keyboardWillHide(notification: NSNotification!) {
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+
 
         // Do any additional setup after loading the view.
     }
@@ -23,6 +40,14 @@ class SignInFormViewController: UIViewController {
 
     @IBAction func backToSegue2(sender: AnyObject) {
         navigationController!.popViewControllerAnimated(true)
+    }
+
+    @IBAction func didTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+
+    @IBAction func onChange(sender: AnyObject) {
+        signInButton.setTitleColor(UIColor.init(red: 0/255, green: 130/255, blue: 227/255, alpha: 1.0), forState: UIControlState.Normal)
     }
     /*
     // MARK: - Navigation
